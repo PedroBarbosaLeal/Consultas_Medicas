7d84fd73d19806011ed30e5f0484f74155c6e9ee
Pedro Leal <80418208+PedroBarbosaLeal@users.noreply.github.com>
2025-08-06 15:29:58 -0300

    Conectando o projeto ao GitHub

---
+++ ConsultasMedicas/src/test/java/com/example/ConsultasMedicas/ConsultasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConsultasMedicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
