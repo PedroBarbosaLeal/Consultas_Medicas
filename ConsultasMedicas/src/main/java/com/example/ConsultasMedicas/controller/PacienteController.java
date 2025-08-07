ec0f9b46e1803ba1ca133f4e754f28c34f4b0e88
Pedro Leal <80418208+PedroBarbosaLeal@users.noreply.github.com>
2025-08-07 08:43:07 -0300

    Criando os Controllers e as classes Service

---
+++ ConsultasMedicas/src/main/java/com/example/ConsultasMedicas/controller/PacienteController.java
@@ -0,0 +1,9 @@
+package com.example.ConsultasMedicas.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Pacientes")
+public class PacienteController {
+}
---
+++ ConsultasMedicas/src/main/java/com/example/ConsultasMedicas/service/PacienteService.java
@@ -0,0 +1,7 @@
+package com.example.ConsultasMedicas.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PacienteService {
+}
