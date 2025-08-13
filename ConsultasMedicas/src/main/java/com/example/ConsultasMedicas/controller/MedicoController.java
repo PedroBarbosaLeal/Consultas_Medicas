f0cbc06d2a0681115ceaca2b8aaf4bfc00a47463
Pedro Leal <80418208+PedroBarbosaLeal@users.noreply.github.com>
2025-08-13 11:20:03 -0300

    'Cadastrar' no controller criado

--- ConsultasMedicas/src/main/java/com/example/ConsultasMedicas/controller/MedicoController.java
+++ ConsultasMedicas/src/main/java/com/example/ConsultasMedicas/controller/MedicoController.java
@@ -1,9 +1,30 @@
 package com.example.ConsultasMedicas.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.ConsultasMedicas.domain.Medico;
+import com.example.ConsultasMedicas.service.MedicoService;
+import com.example.ConsultasMedicas.service.PacienteService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/Medicos")
 public class MedicoController {
+
+    @Autowired
+    private MedicoService repository;
+
+    @PostMapping
+    public ResponseEntity<String> cadastrar(@Valid @RequestBody Medico dados) {
+        Medico medico = repository.cadastrar(dados);
+
+        URI url = URI.create("/Medicos/" + medico.getId_medico());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Medico cadastrado: " + medico.getId_medico() + " " + medico);
+    }
+
 }
