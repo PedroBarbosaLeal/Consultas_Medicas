d2523b52698c4b55b85d637088875a0e178ca99a
Pedro Leal <80418208+PedroBarbosaLeal@users.noreply.github.com>
2025-08-13 11:04:19 -0300

    Deletar Medico

--- ConsultasMedicas/src/main/java/com/example/ConsultasMedicas/service/MedicoService.java
+++ ConsultasMedicas/src/main/java/com/example/ConsultasMedicas/service/MedicoService.java
@@ -4,6 +4,7 @@ import com.example.ConsultasMedicas.domain.Medico;
 import com.example.ConsultasMedicas.domain.repository.MedicoRepository;
 import com.example.ConsultasMedicas.dto.AtualizarMedico;
 import com.example.ConsultasMedicas.infra.exceptions.EsseIdNaoExiste;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,10 +32,19 @@ public class MedicoService {
         return repository.findById(id).orElseThrow(() -> new EsseIdNaoExiste("Médico não encontrado"));
     }
 
+    @Transactional
     public Medico atualizar(long id, AtualizarMedico dados) {
         Medico medico = repository.findById(id).orElseThrow(() -> new EsseIdNaoExiste("Médico não encontrado"));
         medico.atualizarMedico(dados);
         return medico;
     }
 
+    @Transactional
+    public void deletar(Long id){
+        if(repository.existsById(id)){
+            repository.deleteById(id);
+        }else{
+            throw new EsseIdNaoExiste("Esse ID não existe");
+        }
+    }
 }
