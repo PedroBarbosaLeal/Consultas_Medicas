3da21828cd6d2e407f3ecec2fc482cd8282edd7a
Pedro Leal <80418208+PedroBarbosaLeal@users.noreply.github.com>
2025-08-15 15:27:06 -0300

    Tratar Bad Request

--- ConsultasMedicas/src/main/java/com/example/ConsultasMedicas/infra/TratadorDeErros.java
+++ ConsultasMedicas/src/main/java/com/example/ConsultasMedicas/infra/TratadorDeErros.java
@@ -2,8 +2,13 @@ package com.example.ConsultasMedicas.infra;
 import com.example.ConsultasMedicas.infra.exceptions.EsseIdNaoExiste;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class TratadorDeErros {
@@ -11,5 +16,13 @@ public class TratadorDeErros {
     @ExceptionHandler(EsseIdNaoExiste.class)
     public ResponseEntity<String> tratarIdNaoExistente(EsseIdNaoExiste ex){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> TratarBadRequest(MethodArgumentNotValidException ex){
+
+        List<String> erros = ex.getBindingResult().getAllErrors().stream().map(ObjectError::getDefaultMessage).collect(Collectors.toList());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erros);
     }
 }
