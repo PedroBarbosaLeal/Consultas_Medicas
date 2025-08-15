609c05843d6e477038f54f689d7286cfb0978314
Pedro Leal <80418208+PedroBarbosaLeal@users.noreply.github.com>
2025-08-15 11:36:27 -0300

    No ConsultaController o POST e o GET

--- ConsultasMedicas/src/main/java/com/example/ConsultasMedicas/controller/ConsultaController.java
+++ ConsultasMedicas/src/main/java/com/example/ConsultasMedicas/controller/ConsultaController.java
@@ -1,9 +1,40 @@
 package com.example.ConsultasMedicas.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.ConsultasMedicas.domain.Consulta;
+import com.example.ConsultasMedicas.domain.Medico;
+import com.example.ConsultasMedicas.domain.Paciente;
+import com.example.ConsultasMedicas.service.ConsultaService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/Consultas")
 public class ConsultaController {
+
+    @Autowired
+    private ConsultaService repository;
+
+    @PostMapping
+    public ResponseEntity<String> criarConsulta(@Valid @RequestBody Consulta consulta) {
+        Consulta consultaCriada = repository.criarConsulta(consulta);
+
+        URI url = URI.create("/Consultas/" + consultaCriada.getId_consulta());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Consulta Marcada: " + consultaCriada.getId_consulta() + " " + consultaCriada);
+    }
+
+    @GetMapping("/medicos/{id}")
+    public ResponseEntity<Medico> listarConsultasApartirDoMedico(@PathVariable Long id) {
+        return ResponseEntity.ok(repository.listarConsultaPorIdMedico(id));
+    }
+
+    @GetMapping("/paciente/{id}")
+    public ResponseEntity<Paciente> listarConsultasApartirDoPaciente(@PathVariable Long id) {
+        return ResponseEntity.ok(repository.listarConsultaPorIdPaciente(id));
+    }
 }
