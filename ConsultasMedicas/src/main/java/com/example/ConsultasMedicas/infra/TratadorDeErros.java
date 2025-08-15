19610deb1068bf284a67bfb336702956dbf323f0
Pedro Leal <80418208+PedroBarbosaLeal@users.noreply.github.com>
2025-08-15 15:46:07 -0300

    Tratando erro generico 500

--- ConsultasMedicas/src/main/java/com/example/ConsultasMedicas/infra/TratadorDeErros.java
+++ ConsultasMedicas/src/main/java/com/example/ConsultasMedicas/infra/TratadorDeErros.java
@@ -1,5 +1,7 @@
 package com.example.ConsultasMedicas.infra;
+
 import com.example.ConsultasMedicas.infra.exceptions.EsseIdNaoExiste;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,15 +17,20 @@ import java.util.stream.Collectors;
 public class TratadorDeErros {
 
     @ExceptionHandler(EsseIdNaoExiste.class)
-    public ResponseEntity<String> tratarIdNaoExistente(EsseIdNaoExiste ex){
+    public ResponseEntity<String> tratarIdNaoExistente(EsseIdNaoExiste ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<List<String>> TratarBadRequest(MethodArgumentNotValidException ex){
+    public ResponseEntity<List<String>> TratarBadRequest(MethodArgumentNotValidException ex) {
 
         List<String> erros = ex.getBindingResult().getAllErrors().stream().map(ObjectError::getDefaultMessage).collect(Collectors.toList());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erros);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity TratarDuplicidade(DataIntegrityViolationException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
     }
 }
